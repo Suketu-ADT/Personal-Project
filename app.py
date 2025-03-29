79c8258203e77377dd8fdc1c090acf6983803e50
Suketu-ADT <138889078+Suketu-ADT@users.noreply.github.com>
2025-03-29 10:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,29 +32,9 @@ st.write(data.tail())
 
 def plot_raw_data():
     fig = go.Figure()
-    
-    # 1) Stock Open line
-    fig.add_trace(go.Scatter(
-        x=data['Date'],
-        y=data['Open'],
-        name='stock_open',
-        line=dict(color='blue', width=2)
-    ))
-    
-    # 2) Stock Close line
-    fig.add_trace(go.Scatter(
-        x=data['Date'],
-        y=data['Close'],
-        name='stock_close',
-        line=dict(color='red', width=2)
-    ))
-    
-    # 3) Update layout to use default Plotly styling
-    fig.update_layout(
-        title_text=f"{selected_stock} Stock Price Over Time",
-        xaxis_rangeslider_visible=True,   # Show the bottom rangeslider
-        hovermode='x unified'            # Combine hover labels if desired
-        # No 'template', 'plot_bgcolor', or extra styling so we get the default look
-    )
-    
-    st.plotly_chart(fig, use_container_width=True)
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
